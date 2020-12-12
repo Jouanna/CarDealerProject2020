03b84a95ed18a8b09d4e5eca0c5fca4444ba575c
User <User@DESKTOP-MS6O653>
2020-12-12 21:51:44 +0200

    all controllers

--- CarDealerProject2020/Controllers/StoreController.cs
+++ CarDealerProject2020/Controllers/StoreController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarDealerProject2020.Services;
+using CarDealerProject2020.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,114 @@ namespace CarDealerProject2020.Controllers
 {
     public class StoreController : Controller
     {
+        private readonly IStoreService service;
+
+        public StoreController(IStoreService service)
+        {
+            this.service = service;
+        }
         public IActionResult Index()
         {
-            return View();
+            return this.RedirectToAction("All");
+        }
+
+        public IActionResult All()
+        {
+            var viewModel = this.service.All();
+            return this.View(viewModel);
+        }
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(StoreCreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                this.service.Create(model);
+                return this.RedirectToAction("All");
+            }
+            return this.View(model);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = this.service.Details(id);
+
+            if (viewModel == null)
+            {
+                return NotFound();
+            }
+
+            return this.View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(StoreEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                this.service.Edit(model);
+                return this.RedirectToAction("All");
+            }
+            return this.View(model);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = this.service.Details(id);
+
+            if (viewModel == null)
+            {
+                return NotFound();
+            }
+
+            return this.View(viewModel);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = this.service.Details(id);
+
+            if (viewModel == null)
+            {
+                return NotFound();
+            }
+
+            return this.View(viewModel);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirm(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            this.service.Delete(id);
+            return this.RedirectToAction("All");
         }
     }
 }
