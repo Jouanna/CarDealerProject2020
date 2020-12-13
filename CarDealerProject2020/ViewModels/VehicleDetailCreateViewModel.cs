38fd0f1b36c0c6a4ffc2c98ed1bde895f99e2c85
User <User@DESKTOP-MS6O653>
2020-12-13 18:41:28 +0200

    views

--- CarDealerProject2020/ViewModels/VehicleDetailCreateViewModel.cs
+++ CarDealerProject2020/ViewModels/VehicleDetailCreateViewModel.cs
@@ -9,5 +9,15 @@ namespace CarDealerProject2020.ViewModels
     {
         public int DetailId { get; set; }
         public int VehicleId { get; set; }
+
+        public string BrandName { get; set; }
+        public string Type { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+        public string Fuel { get; set; }
+        public string HorsePower { get; set; }
+        public string Color { get; set; }
+        public string Price { get; set; }
+        public string StoreId { get; set; }
     }
 }
--- CarDealerProject2020/ViewModels/VehicleDetailEditViewModel.cs
+++ CarDealerProject2020/ViewModels/VehicleDetailEditViewModel.cs
@@ -10,5 +10,15 @@ namespace CarDealerProject2020.ViewModels
         public int Id { get; set; }
         public int DetailId { get; set; }
         public int VehicleId { get; set; }
+
+        public string BrandName { get; set; }
+        public string Type { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+        public string Fuel { get; set; }
+        public string HorsePower { get; set; }
+        public string Color { get; set; }
+        public string Price { get; set; }
+        public string StoreName { get; set; }
     }
 }
